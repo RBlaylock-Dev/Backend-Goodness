be54443996763eddad039448f9597ca25ebfe468
Robert Blaylock <rblaylock.prsvr@gmail.com>
2025-01-09 19:05:59 -0600

    test

--- template/server.js
+++ template/server.js
@@ -10,6 +10,7 @@ app.use(express.json()); // Middleware to parse JSON body
 // Define a port
 const PORT = 3000;
 
+
 // Define a simple route
 app.get('/', (req, res) => {  //req, res cycle
     console.log("GET route hit / - ")
